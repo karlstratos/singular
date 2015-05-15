657b0a52bcdd85f8e3126b7a493d4c2c6b55b6c0
stratos <karlstratos@gmail.com>
2015-05-15 18:04:23 -0400

    cleaning

--- src/wordrep.cc
+++ src/wordrep.cc
@@ -779,14 +779,12 @@ void WordRep::TestQualityOfWordVectors() {
     string wordsim353_path = "third_party/public_datasets/wordsim353.dev";
     string men_path = "third_party/public_datasets/men.dev";
     string rw_path = "third_party/public_datasets/rw.dev";
-    string mturk_path = "third_party/public_datasets/mturk.dev";
     string syn_path = "third_party/public_datasets/syntactic_analogies.dev";
     string mixed_path = "third_party/public_datasets/mixed_analogies.dev";
     FileManipulator file_manipulator;
     if (!file_manipulator.Exists(wordsim353_path) ||
 	!file_manipulator.Exists(men_path) ||
 	!file_manipulator.Exists(rw_path) ||
-	!file_manipulator.Exists(mturk_path) ||
 	!file_manipulator.Exists(syn_path) ||
 	!file_manipulator.Exists(mixed_path)) {
 	// Skip evaluation (e.g., in unit tests) if files are not found.
@@ -826,15 +824,6 @@ void WordRep::TestQualityOfWordVectors() {
 				&num_handled_rw, &corr_rw);
     log_ << "   RW:    \t" << corr_rw << " (" << num_handled_rw << "/"
 	 << num_instances_rw << " evaluated)" << endl;
-
-    // Word similarity with mturk.dev.
-    size_t num_instances_mturk;
-    size_t num_handled_mturk;
-    double corr_mturk;
-    eval.EvaluateWordSimilarity(wordvectors_, mturk_path, &num_instances_mturk,
-				&num_handled_mturk, &corr_mturk);
-    log_ << "   MTURK: \t" << corr_mturk << " (" << num_handled_mturk << "/"
-	 << num_instances_mturk << " evaluated)" << endl;
 
     // Word analogy with syntactic_analogies.dev.
     log_ << fixed << setprecision(2);
